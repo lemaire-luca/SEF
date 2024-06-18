34eda6cd1558dd14b39404a572275a281a7a4ff0
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-18 13:34:28 +0200

    adding role

--- wp-content/themes/SEF/template-confidential.php
+++ wp-content/themes/SEF/template-confidential.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 <section id="confidential" class="confidential">
     <div class="confidential__title">
-        <h2>Politique de confidentialité </h2>
+        <h2 role="heading" aria-level="2">Politique de confidentialité</h2>
         <p>Dernière mise à jour le : <span><?= get_field('confidential-update')?></span></p>
     </div>
     <div class="confidential__container">
@@ -13,7 +13,7 @@
                 $title = get_sub_field('title');
                 ?>
             <div class="confidential__container__infos">
-                <h3><?= $title?></h3>
+                <h3 role="heading" aria-level="3"><?= $title?></h3>
                 <p><?= $text?></p>
             </div>
             <?php endwhile;
