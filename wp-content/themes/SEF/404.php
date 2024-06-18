964e985d023a9738541186d3e04891887ccab810
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-18 13:40:11 +0200

    adding role

--- wp-content/themes/SEF/404.php
+++ wp-content/themes/SEF/404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-    <main>
+    <main role="main">
         <h2 class="hidden" role="heading" aria-level="2">Error404</h2>
         <section class="error">
             <div class="error__content">
-                <h3>Erreur 404</h3>
+                <h3 role="heading" aria-level="3">Erreur 404</h3>
                 <p>Page introuvable !</p>
                 <div class="error__content__button">
                     <a class="button-error" href="<?= get_field('error-home-link', 'options'); ?>">Retour à
