e64da773f28c6aa925f41d7d1f2978acf37bf18c
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-18 13:33:48 +0200

    adding role and micro data

--- wp-content/themes/SEF/template-contact.php
+++ wp-content/themes/SEF/template-contact.php
@@ -4,12 +4,12 @@
     <section id="contact" class="contact">
         <div class="contact__information">
             <h2 role="heading" aria-level="2">Contactez-nous</h2>
-            <div class="contact__information__link">
+            <div itemscope itemprop="https://schema.org/Organization" class="contact__information__link">
                 <p>Adresse : Rue du Marché 33 4500 HUY</p>
-                <a href="tel:085 / 21 57 52">
+                <a itemprop="telephone" href="tel:085 / 21 57 52">
                     <p>Telephone : <span class="num">085 / 21 57 52</span></p>
                 </a>
-                <a href="mailto:contact@sefasbl.com">
+                <a itemprop="email" href="mailto:contact@sefasbl.com">
                     <p>Email : <span class="mail">contact@sefasbl.com</span></p>
                 </a>
             </div>
